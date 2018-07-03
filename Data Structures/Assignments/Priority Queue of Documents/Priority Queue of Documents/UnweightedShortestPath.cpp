c33b527ed2c5b08b0e7ca0f93e93f9af477b1cd1
robby06 <40376728+robby06@users.noreply.github.com>
2018-07-02 23:40:32 -0700

    Add files via upload

---
+++ Data Structures/Assignments/Priority Queue of Documents/Priority Queue of Documents/UnweightedShortestPath.cpp
@@ -0,0 +1,160 @@
+/*
+	Written by Robby Sanchez
+	Assignment 6: (Extra Credit) Priority Queue of Documents
+*/
+// FILE: UnweightedShortestPath.cpp
+#include <iostream>
+#include<cstring>
+#include "graph.h"
+using namespace std;
+
+int main() {
+	graph Map;
+	int first, second;
+	char column1;
+	char column2;
+#ifdef TRAFFIC
+	cout << "TRAFFIC RUNNING..." << endl;
+#endif
+	while (true) {
+
+#ifdef TRAFFIC
+		cout << "Enter Q to quit\n";
+		cout << "From intersection  <enter column from A to D>: ";
+		cin >> column1;
+		if (column1 == 'Q') {
+			cout << "Illegal entry\n";
+			break;
+		}
+
+		if (!(column1 == 'A' || column1 == 'B' || column1 == 'C'
+				|| column1 == 'D')) {
+			cout << "Illegal entry\n";
+			continue;
+		}
+		cout << "<enter row from 1 to 4>: ";
+		cin >> first;
+		--first;
+		if (first > 4 || first < -1) {
+			cout << "Illegal entry\n";
+			continue;
+
+		}
+		switch (column1) {
+		case 'B':
+			first = 4 + first;
+			break;
+		case 'C':
+			first = 8 + first;
+			break;
+		case 'D':
+			first = 12 + first;
+			break;
+		default:
+			break;
+		}
+#endif
+#ifdef BOOK_SAMPLE // for textbook sample
+		cout << "Enter 0 to quit\n";
+		cout << "From Vertex (enter number from 1 to " << Map.size() << "): ";
+		cin >> first;
+		--first; // set actual index
+#endif
+
+#ifdef AIRLINE_SAMPLE // for airline sample
+		cout << "Enter -1 to quit\n";
+		cout << "From city (enter number from 0 to " << Map.size() - 1 << "): ";
+		cin >> first;
+#endif
+		if (first >= Map.size() || first < -1) {
+			cout << "Illegal entry\n";
+			continue;
+		}
+		if (first == -1)
+			break;
+		cout << "From " << Map.Vertex_Names[first]
+				<< " to intersection  <enter column from A to D>: ";
+#ifdef BOOK_SAMPLE // for textbook sample
+		cout << " To Vertex? (from 1 to " << Map.size() << "): ";
+		cin >> second;
+		--second; // set actual index
+#endif
+#ifdef AIRLINE_SAMPLE // for airline sample
+		cout << " to city? (from 0 to " << Map.size() - 1 << "): ";
+		cin >> second;
+#endif
+#ifdef TRAFFIC
+		cin >> column2;
+
+		if (!(column2 == 'A' || column2 == 'B' || column2 == 'C'
+				|| column2 == 'D')) {
+			cout << "Illegal entry\n";
+			continue;
+		}
+		cout << "<enter row from 1 to 4>: ";
+		cin >> second;
+		second--;
+
+		switch (column2) {
+		case 'B':
+			second = 4 + second;
+			break;
+		case 'C':
+			second = 8 + second;
+			break;
+		case 'D':
+			second = 12 + second;
+			break;
+		default:
+			break;
+		}
+
+#endif
+
+		if (second >= Map.size() || second < 0) {
+			cout << "Illegal entry\n";
+			continue;
+		} else
+			cout << Map.Vertex_Names[second] << endl; // display destination
+		Map.compute_itinerary(first, second);
+	} // end of while loop
+
+	system("pause");
+	return 0;
+}
+// ************My output******
+/*
+TRAFFIC RUNNING...
+Enter Q to quit
+From intersection  <enter column from A to D>: A
+<enter row from 1 to 4>: 1
+From A1 to intersection  <enter column from A to D>: B
+<enter row from 1 to 4>: 2
+B2
+
+Number of elements n: 16
+Size*size  (n*n) = 256
+Total number of edges = 30
+Iterations = 6
+The following itinerary is suggested:
+A1 - A2 - B2
+Number of edges required: 2
+
+Enter Q to quit
+From intersection  <enter column from A to D>: D
+<enter row from 1 to 4>: 4
+From D4 to intersection  <enter column from A to D>: A
+<enter row from 1 to 4>: 3
+A3
+
+Number of elements n: 16
+Size*size  (n*n) = 256
+Total number of edges = 30
+Iterations = 29
+The following itinerary is suggested:
+D4 - D3 - D2 - C2 - A2 - A3
+Number of edges required: 5
+
+Enter Q to quit
+From intersection  <enter column from A to D>:
+*/
